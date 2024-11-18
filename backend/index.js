a80afe68f4a76cf6718b93d3c0d15b851d98d7f9
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-11-18 10:56:51 +0530

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config()
 
-const port = 3000;
+const port = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URI;
+
+try {
+  mongoose.connect(MONGO_URL);
+  console.log("connected to MONGODB");
+} catch {
+  console.log(error);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
