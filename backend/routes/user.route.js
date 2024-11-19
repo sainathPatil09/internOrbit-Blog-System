45c59b546314f42c0fd561ae1c297dc5b5076b6d
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-11-19 12:49:29 +0530

    code to login user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { register } from '../controller/user.controller.js';
+import { login, register } from '../controller/user.controller.js';
 
 const router = express.Router();
 
 router.post("/register", register); 
+router.post("/login", login);
 
 export default router
